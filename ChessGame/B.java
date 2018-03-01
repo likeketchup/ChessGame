5043c0112a696959db4c47cd14340cae5406b8e6
LichiLiu <13520617031@163.com>
2018-03-02 06:49:52 +0800

    ..

---
+++ ChessGame/B.java
@@ -0,0 +1,12 @@
+
+public class B extends piece
+{
+
+    B(String color,int X, int Y)
+    {
+        super("B",color,X,Y);
+    }
+    public void move(int x, int y)
+    {
+    }
+}
---
+++ ChessGame/Q.java
@@ -0,0 +1,12 @@
+
+public class Q extends piece
+{
+
+    Q(String color,int X, int Y)
+    {
+        super("Q",color,X,Y);
+    }
+    public void move(int x, int y)
+    {
+    }
+}
