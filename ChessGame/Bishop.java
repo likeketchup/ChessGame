9f4fde4ff2f2de271b403dabe1e89914d93cc4ea
LichiLiu <13520617031@163.com>
2018-03-02 09:15:18 +0800

    lll

---
+++ ChessGame/Bishop.java
@@ -0,0 +1,12 @@
+
+public class Bishop extends piece
+{
+    String name = "Bishop";
+    Bishop(String color,int X, int Y)
+    {
+        super(color,X,Y);
+    }
+    public void move(int x, int y)
+    {
+    }
+}
---
+++ ChessGame/Queen.java
@@ -0,0 +1,12 @@
+
+public class Queen extends piece
+{
+    String name = "Queen";
+    Queen(String color,int X,int Y)
+    {
+        super(color,X,Y);
+    }
+    public void move(int x, int y)
+    {
+    }
+}
