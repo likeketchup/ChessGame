42acc3f2b7db67af1c12c2d298bab7f510c2be6b
MythSama <1720378663@qq.com>
2018-03-02 09:24:11 +0800

    last pieces

---
+++ ChessGame/Pawn.java
@@ -0,0 +1,13 @@
+
+public class Pawn extends piece
+{
+
+	String name = "Pawn";
+    Pawn(String color,int X, int Y)
+    {
+        super(color,X,Y);
+    }
+    public void move(int x, int y)
+    {
+    }
+}
