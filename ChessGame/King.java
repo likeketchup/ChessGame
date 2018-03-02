91715869cf981cc9ce4e5c961f7bdad8fd199ebb
MythSama <1720378663@qq.com>
2018-03-02 09:07:14 +0800

    King

---
+++ ChessGame/King.java
@@ -0,0 +1,13 @@
+
+public class King extends piece
+{
+
+	String name = "King";
+    Knight(String color,int X, int Y)
+    {
+        super(color,X,Y);
+    }
+    public void move(int x, int y)
+    {
+    }
+}
