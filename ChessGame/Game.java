321ed1c8a6bc2eda11080f657e986209e2effeb9
LichiLiu <13520617031@163.com>
2018-03-21 09:45:17 +0800

    I write game
    Signed-off-by: LichiLiu <13520617031@163.com>

---
+++ ChessGame/Game.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Game
+{
+
+    public static void main(String[] args){
+
+        Scanner scn=new Scanner(System.in);
+        String continuee="";
+
+        while(!continuee.equals("yy"))
+        {
+            System.out.println("Do you want to start a game? (y/n)"+"["+"whitePlayer"+"]");
+            continuee=scn.nextLine();
+            System.out.println("Do you want to start a game? (y/n)"+"["+"blackPlayer"+"]");
+            continuee+=scn.nextLine();
+        }
+
+        Board b=Board.factory();
+        System.out.println("Enter the name of User 1（white）:");
+        String firstName=scn.nextLine();
+        Player whitePlayer=Player.factory(firstName,b);
+        System.out.println("Enter the name of User 2(black):");
+        String secondName=scn.nextLine();
+        Player blackPlayer=Player.factory(secondName,b);
+        System.out.println("\n");
+
+        while(b.judgment())
+        {
+            System.out.println("White play");
+            System.out.print("Move piece from (");String previousOne=scn.nextLine();System.out.print(",");String previousTwo=scn.nextLine();System.out.print(") to (");
+            String laterOne=scn.nextLine();System.out.print(",");String laterTwo=scn.nextLine();System.out.println(")");
+            whitePlayer.movePiece(previousOne+previousTwo, laterOne+laterTwo);
+
+            System.out.println("Black play");
+            System.out.print("Move piece from (");previousOne=scn.nextLine();System.out.print(",");previousTwo=scn.nextLine();System.out.print(") to (");
+            laterOne=scn.nextLine();System.out.print(",");laterTwo=scn.nextLine();System.out.println(")");
+            blackPlayer.movePiece(previousOne+previousTwo, laterOne+laterTwo);
+
+            System.out.println(b);
+        }
+
+
+        System.out.println("game over");
+
+
+    }
+}
