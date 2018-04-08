6de1d7e31e5dfdc207b4c712abca874febd5c941
Toma Chen <likeketchup@outlook.com>
2018-04-08 18:05:19 +0800

    small caase

--- ChessGame/Player.java
+++ ChessGame/Player.java
@@ -45,11 +45,10 @@ public class Player
     }
     //please notice that the first section of the coordinate in our project is Y!
     public int[] translator(String p){
-        if(p.charAt(0)<65||p.charAt(0)>72||p.charAt(1)<49||p.charAt(1)>56) throw new RuntimeException("Input illegal!");
         int[] coordinate = new int[2];
         coordinate[0] = Math.abs(Integer.parseInt(p.substring(1,2))-1);//y
         coordinate[0] = Math.abs(coordinate[0]-7);
-        coordinate[1] = ((int)p.charAt(0))-65;//x
+        coordinate[1] = ((int)p.charAt(0))-97;//x
         return coordinate;
     }
 
